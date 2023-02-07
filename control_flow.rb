f94909dffb3d945e069ae3262c6b7a371f39fe52
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-07 08:18:22 +0300

    Add case to #calculator. All tests passing.

--- control_flow.rb
+++ control_flow.rb
@@ -31,5 +31,14 @@ end
 
 def calculator(operation, num1, num2)
   # your code here
+  
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+
+  else puts "Invalid operation!"
+  end
 end
 
